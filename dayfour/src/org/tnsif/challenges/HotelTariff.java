d709d534d6a9046618dcf9f7258f73404d411043
SanskrutiKukkar <sanskrutikukkar@gmail.com>
2023-08-01 11:59:00 +0530

    first commit

---
+++ dayfour/src/org/tnsif/challenges/HotelTariff.java
@@ -0,0 +1,30 @@
+package org.tnsif.challenges;
+import java.util.Scanner;
+public class HotelTariff {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int month=s.nextInt();
+		float rent=s.nextFloat();
+		int days=s.nextInt();
+		switch(month)
+		{
+		  case 1:
+		  case 2:
+		  case 3:
+		  case 7:
+		  case 8:
+		  case 9:
+		  case 10:System.out.println(rent*days);
+		          break;
+		  case 1:
+		  case 1:
+		  case 1:
+		  case 1:
+		  case 1:
+		}
+		
+
+	}
+
+}
---
+++ dayfour/src/org/tnsif/challenges/MicrowaveHeatingTime.java
@@ -0,0 +1,10 @@
+package org.tnsif.challenges;
+import java.util.Scanner;
+public class MicrowaveHeatingTime {
+
+	public static void main(String[] args) {
+		int item=
+
+	}
+
+}
